113ff4940ff3f8cb19caabdc3127a7110ae70c2d
lastelementatnight <lastelementatnight@proton.me>
2024-06-05 18:15:24 +0200

    Error: no table: loop view

--- scanwifi.py
+++ scanwifi.py
@@ -15,6 +15,8 @@ import signal
 import os
 import nic_service as nics
 import sniffing_service as sniffs
+import time
+import pandas
 from threading import Thread
 from scapy.layers.dot11 import Dot11, Dot11Beacon, Dot11ProbeResp, Dot11ProbeReq, Dot11Elt, sniff, RadioTap
 
@@ -31,28 +33,52 @@ def check_permissions():
         print("No permissions. Run as root!")
         exit()
 
+wlan_list = pandas.DataFrame(columns=["BSSID", "SSID", "channel", "crypto"])
+wlan_list.set_index("BSSID", inplace=True)
+
+def eval_wifi_ap_packets(packet):
+    if packet.haslayer(Dot11Beacon) or packet.haslayer(Dot11ProbeResp):
+        # if packet.type == 0 and packet.subtype == 8:
+            print("Start sniffing...")
+            bssid = packet[Dot11].addr2
+            ssid = packet[Dot11Elt].info.decode().strip()
+            net_stats = packet[Dot11Beacon].network_stats()
+            channel = net_stats.get("channel")
+            protocol = net_stats.get("crypto")
+            
+            wlan_list.loc[bssid] = (ssid, channel, protocol)
+
+def print_tabel():
+    while True:
+        os.system("clear")
+        print(wlan_list)
+        time.sleep(0.5)
+
+
 def run_app():
     ### this run app
+    return None
+    
 
+if __name__ == "__main__":
+    signal.signal(signal.SIGINT, keybord_interrupt_handler)
     check_permissions()
-
+    
+    printer_for_tabel = Thread(target=print_tabel)
+    printer_for_tabel.daemon = True
+    printer_for_tabel.start()
+    
     wlan_nic = nics.list_interfaces()
-
-    for nic in wlan_nic:
-        print(nic)
 
     os.system(f"sudo airmon-ng check kill")
     nics.monitor_mode(wlan_nic[0])
     
-    channel_changer = Thread(target=lambda: nics.change_wifi_channel(wlan_nic[0]))
+    # channel_changer = Thread(target=lambda: nics.change_wifi_channel(wlan_nic[0]))
+    channel_changer = Thread(target=eval_wifi_ap_packets)
     channel_changer.daemon = True
     channel_changer.start()
     
-    sniff(prn=sniffs.eval_wifi_ap_packets, iface=wlan_nic[0])
+    sniff(prn=eval_wifi_ap_packets, iface=wlan_nic[0], store=0)
     
     os.system(f"sudo systemctl start NetworkManager.service")
     nics.managed_mode(wlan_nic[0])
-
-if __name__ == "__main__":
-    signal.signal(signal.SIGINT, keybord_interrupt_handler)
-    run_app()
